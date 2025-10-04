241857d723845c8987c3d277d34ec2c8f03578ab
Joey Salzmann (Deck) <joey.salzmann@gmail.com>
2025-10-05 00:17:16 +0200

    Moved Scheduler to Bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         //commands: __DIR__.'/../routes/console.php',
         health: '/laravel-health',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        // Division Sessions Sync - Every 15 minutes
+        $schedule->command('division_sessions:sync')
+            ->everyFifteenMinutes()
+            ->withoutOverlapping()
+            ->onOneServer()
+            ->runInBackground();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin'             => \App\Http\Middleware\CheckAdmin::class,
