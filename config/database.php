0775a9932b6a2d6f9a544b788cdd6bf5a0870e6d
Joey Salzmann (Deck) <joey.salzmann@gmail.com>
2025-10-04 19:03:32 +0200

    Wiring Awards DB

--- config/database.php
+++ config/database.php
@@ -112,6 +112,22 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'awards_db' => [
+            'driver' => 'mysql',
+            'host' => env('AWARDS_DB_HOST', '127.0.0.1'),
+            'port' => env('AWARDS_DB_PORT', '3306'),
+            'database' => env('AWARDS_DB_DATABASE', 'secondary'),
+            'username' => env('AWARDS_DB_USERNAME', 'root'),
+            'password' => env('AWARDS_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
